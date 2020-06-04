337c1826e2ed5e53ee439c8c84a011d0fab6cbb7
Busso Luciano <busso.luciano@hotmail.com>
2020-06-04 15:14:18 -0300

    final recuperado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu.js';
+import Mensaje from './Mensaje.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      titulo : ['Samsung S11'],
+      campos : [{etiqueta:'Descripcion: ', data:'Ultimo modelo de Samsung en Argentina'},{etiqueta:'Precio: ', data:'$ 29.999'},{etiqueta:'SKU: ', data:'QUJ56JH'},{etiqueta:'Cantidad Disponible: ', data:'128'}],
+      mensaje : ['']
+    }
+    this.handleClick=this.handleClick.bind(this)
+  }
+  handleClick = () => {
+    this.setState ({
+      mensaje : ['Gracias por su compra.']
+    })
+  }
+
+  
+  render () {
+    return(
+      <div>
+        <Menu
+        titulo = {this.state.titulo}
+        campos = {this.state.campos}/>
+        <button onClick={this.handleClick}> Comprar </button>
+        <Mensaje
+        mensaje = {this.state.mensaje}/>
+    </div>    
+    )
+  }
 }
 
 export default App;
